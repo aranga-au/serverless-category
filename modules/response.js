b53caf9d6abc8d938848c377970603914c796cf1
aranga-au <aranga.nanayakkara@gmail.com>
2016-11-02 23:32:43 +1100

    response

---
+++ modules/response.js
@@ -0,0 +1,20 @@
+var resonse = {
+
+    create:createResponse
+
+};
+
+function  createResponse(response,status) {
+
+    if (!status){
+        status = '200';
+    }
+
+    return {
+        statusCode: status,
+        body: JSON.stringify(response),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+}
